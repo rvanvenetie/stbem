077c8c57430e826ed47c12290d8aad91280b71ce
Raymond van Venetie <rayvanve@gmail.com>
2021-05-19 16:24:49 +0200

    removed exact

--- results.sh
+++ results.sh
@@ -11,8 +11,8 @@ estim_quadrature=5355
 if [ $refinement = 'uniform' ]
 then
     python3 example.py --problem $problem --domain $domain --theta $theta --refinement $refinement --estimator $estimator --estimator-quadrature ${estim_quadrature} \
-        > results_exact/${problem}_${domain}_${refinement}_${estim_quadrature}.log
+        > results/${problem}_${domain}_${refinement}_${estim_quadrature}.log
 else
     python3 example.py --problem $problem --domain $domain --theta $theta --refinement $refinement --estimator $estimator --estimator-quadrature ${estim_quadrature} \
-        > results_exact/${problem}_${domain}_${refinement}_${estimator}_${theta}_${estim_quadrature}.log
+        > results/${problem}_${domain}_${refinement}_${estimator}_${theta}_${estim_quadrature}.log
 fi
