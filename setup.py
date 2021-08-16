37cee7022982c6da9e1d13bb4117cddb8510da59
Raymond van Venetie <rayvanve@gmail.com>
2021-08-16 18:38:06 +0200

    Cleaned up source code. Created better file structure for release.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+# This file optionally cythonizes the source code, to gain some additional
+# speedup. Do not forget to recompile if you change the source.
+#
 # Run as `python3 setup.py build_ext --inplace`
 import os
-from glob import glob
-from setuptools import setup
+
 from Cython.Build import cythonize
+from setuptools import setup
 
 os.environ['CFLAGS'] = '-O3'
 setup(ext_modules=cythonize(
     [
-        'quadrature_rules.py', 'quadrature.py', 'parametrization.py',
-        'single_layer_exact.py', 'initial_potential.py', 'error_estimator.py',
-        'hierarchical_error_estimator.py', 'mesh.py', 'initial_mesh.py',
-        'parametrization.py', 'norms.py', 'single_layer.py'
+        'src/quadrature_rules.py', 'src/quadrature.py',
+        'src/parametrization.py', 'src/single_layer_exact.py',
+        'src/initial_potential.py', 'src/error_estimator.py',
+        'src/hierarchical_error_estimator.py', 'src/mesh.py',
+        'src/initial_mesh.py', 'src/parametrization.py', 'src/norms.py',
+        'src/single_layer.py'
     ],
     compiler_directives={'language_level': "3"},
 ))
